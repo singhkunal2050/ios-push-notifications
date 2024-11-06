ea868ebac8f259cfe99057798109c6977e1d6a40
singhkunal2050 <singhkunal2050@gmail.com>
2024-11-06 16:40:35 +0530

    Add logs for service worker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,5 +1,8 @@
 self.addEventListener("push", async (event) => {
-  const { title, body } = await event.data.json();
+  const data = await event.data.json();
+  const title = data.title;
+  const body = data.body;
+  console.log({ data });
   console.log(title, body);
   self.registration.showNotification(title, {
     body,
